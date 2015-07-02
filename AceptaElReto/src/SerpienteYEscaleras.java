22d7b314ef8c65c5df37615be108f7df20f83ab5
Mario <mariovivasdavid@hotmail.es>
2015-07-02 10:39:59 +0200

    Primera versión completa del ejercicio a falta de ser testeada

--- AceptaElReto/src/SerpienteYEscaleras.java
+++ AceptaElReto/src/SerpienteYEscaleras.java
@@ -10,10 +10,15 @@ public class SerpienteYEscaleras {
     public static void main (String[] args) {
         Scanner in = new Scanner(System.in);
         boolean terminado = false;
+        boolean terminadoMeta = false;
+        boolean haySerpiente;
         short[] datos = new short[4];
         short[] casillasSerpiente;
         short[][] escalera; //Se guarda casilla inicial e incremento
-        short aux;
+        short casillaActual;
+        short movimientos;
+        short mejorEscalera;
+        short mejorImpulso;
         
         while(!terminado){
             terminado = true;
@@ -40,10 +45,52 @@ public class SerpienteYEscaleras {
                 //Guardar Escaleras
                 for(byte i = 0; i < datos[3]; i++){
                     escalera[i][0] = in.nextShort();
-                    aux = in.nextShort();
-                    escalera[i][1] = (short)(aux-escalera[i][0]);
+                    escalera[i][1] = in.nextShort();
                 }
-                System.out.println("Datos leidos");
+                
+                //Inicializar y comenzamos a calcular movimienos
+                casillaActual = 1;
+                terminadoMeta = false;
+                movimientos = 0;
+                
+                while(!terminadoMeta){
+                    if(casillaActual == datos[0] * datos[0])
+                        break;
+                    //Elegir mejor escalera en rango de tiro
+                    mejorImpulso = mejorEscalera = -1;
+                    for(byte i = 0; i < escalera.length; i++){
+                        if(escalera[i][0] <= casillaActual+datos[1] 
+                                && escalera[i][1] > mejorImpulso){
+                            mejorEscalera = i;
+                            mejorImpulso = escalera[i][1];
+                        }
+                    }
+                    //Si hay escalera
+                    if(mejorEscalera != -1){
+                        casillaActual = escalera[mejorEscalera][1];
+                        movimientos++;
+                    }
+                    //Si no, mover a casilla más lejana sin serpiente
+                    else{
+                        for(int i = casillaActual + datos[1]; i > casillaActual;i--){
+                            haySerpiente = false;
+                            for(int j = 0; j < casillasSerpiente.length; j++){
+                                if(casillasSerpiente[j] == i){
+                                    haySerpiente = true;
+                                    break;
+                                }
+                            }
+                            if(!haySerpiente){
+                                casillaActual = (short) i;
+                                movimientos++;
+                                break;
+                            }
+                        }
+                    }
+                }
+                
+                System.out.println(movimientos);
+                
             }
         }
         
