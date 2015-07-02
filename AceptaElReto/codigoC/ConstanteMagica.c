6c103fa4a2a5515928ae5c757f5cb290f87d21c8
Miguel <Miguel@Miguel-PC>
2015-07-02 13:35:33 +0200

    Variaciones en ConstanteMagica para C, codigo funcional

--- AceptaElReto/codigoC/ConstanteMagica.c
+++ AceptaElReto/codigoC/ConstanteMagica.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <math.h>
 
+int main ()
+{
+    float n, k;
+
+    while(1)
+    {
+        scanf("%f %f", &n, &k);
+        if (n != 0 || k != 0 )
+            printf("%.0f\n", ((pow(n, 3))+n)/2 + (--k*n));
+        else
+            break;
+    }
+
+    return 0;
+}
